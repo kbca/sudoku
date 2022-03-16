5c08324b82a7e8fe66501c381a1f364aaa29b5c6
heudon@gmail.com <heudon@gmail.com>
2022-03-16 19:48:14 +0100

    Defining coordinates for cells

--- domain/board.go
+++ domain/board.go
@@ -1,6 +1,8 @@
 package domain
 
-import "math/rand"
+import (
+	"math/rand"
+)
 
 type cell struct {
 	coordX int
@@ -12,7 +14,23 @@ type Board struct {
 	cells [81]cell
 }
 
+func (board *Board) defineCoords() {
+	currentX, currentY := 1, 1
+	for i := range board.cells {
+		board.cells[i].coordX = currentX
+		board.cells[i].coordY = currentY
+
+		currentX++
+		if currentX > 9 {
+			currentX = 1
+			currentY++
+		}
+	}
+}
+
 func (board *Board) RandomizeBoard() {
+	board.defineCoords()
+
 	for i := range board.cells {
 		board.cells[i].number = rand.Intn(9) + 1
 	}
