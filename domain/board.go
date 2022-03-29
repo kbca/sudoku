b4659ab571f20f46f3b4db4f41561d8b411e9d95
heudon@gmail.com <heudon@gmail.com>
2022-03-30 00:11:22 +0200

    Setting up blocks coordinates pointers on board

--- domain/board.go
+++ domain/board.go
@@ -1,6 +1,7 @@
 package domain
 
 import (
+	"math"
 	"math/rand"
 )
 
@@ -14,16 +15,19 @@ type Board struct {
 	cells   [81]cell
 	lines   [9][9]*cell
 	columns [9][9]*cell
+	blocks  [9][9]*cell
 }
 
 func (board *Board) defineCoords() {
 	currentLine, currentColumn := 1, 1
+
 	for i := range board.cells {
 		board.cells[i].line = currentLine
 		board.cells[i].column = currentColumn
 
 		board.lines[currentLine-1][currentColumn-1] = &board.cells[i]
 		board.columns[currentColumn-1][currentLine-1] = &board.cells[i]
+		board.blocks[getBlockByCoord(currentLine, currentColumn)-1][getBlockCellByCoord(currentLine, currentColumn)-1] = &board.cells[i]
 
 		currentLine++
 		if currentLine > 9 {
@@ -31,6 +35,54 @@ func (board *Board) defineCoords() {
 			currentColumn++
 		}
 	}
+}
+
+func getBlockByCoord(line int, column int) int {
+	if line <= 3 {
+		return int(math.Ceil(float64(column) / 3.0))
+	}
+
+	if line <= 6 {
+		return int(math.Ceil(float64(column)/3.0)) + 3
+	}
+
+	return int(math.Ceil(float64(column)/3.0)) + 6
+}
+
+func getBlockCellByCoord(line int, column int) int {
+	if line == 1 || line == 4 || line == 7 {
+		if column <= 3 {
+			return column
+		}
+
+		if column <= 6 {
+			return column - 3
+		}
+
+		return column - 6
+	}
+
+	if line == 2 || line == 5 || line == 8 {
+		if column <= 3 {
+			return column + 3
+		}
+
+		if column <= 6 {
+			return column
+		}
+
+		return column - 3
+	}
+
+	if column <= 3 {
+		return column + 6
+	}
+
+	if column <= 6 {
+		return column + 3
+	}
+
+	return column
 }
 
 func (board *Board) RandomizeBoard() {
